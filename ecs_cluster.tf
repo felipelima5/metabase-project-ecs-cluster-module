3b1ea59bfc7202a58eb2c5fb9c0451c176f3feee
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-22 23:04:54 -0300

    add block to redirect logging to cloud watch service

--- ecs_cluster.tf
+++ ecs_cluster.tf
@@ -1,4 +1,18 @@
 resource "aws_ecs_cluster" "this" {
   name = var.ecs_cluster_name
 
+  configuration {
+    execute_command_configuration {
+
+      logging    = var.logging
+      
+      log_configuration {
+        cloud_watch_encryption_enabled = var.cloud_watch_encryption_enabled
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.this.name
+      }
+
+    }
+
+  }
+
 }
